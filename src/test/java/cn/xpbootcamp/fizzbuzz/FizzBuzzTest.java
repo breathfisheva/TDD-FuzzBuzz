e2089304290692fcb7f988ea0c2fba2c3eeb78ea
Hong Liu <hong.liu1@Hongs-MacBook-Pro.local>
2020-05-09 22:54:25 +0800

    add TDD test cases according new requirements

--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -4,10 +4,140 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+	
+	@Test
+	public void should_show_fizz_when_countoff_given_contains7_divided_by3_not_divided_by7() {
+        
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
+        Assert.assertEquals("Fizz", fizzBuzz.convert(27));
+        
+	}
+	
+	@Test
+	public void should_show_Whizz_when_countoff_given_contains7_divided_by7_not_divided_by3() {
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	     Assert.assertEquals("Whizz", fizzBuzz.convert(77));
+		
+	}
+	
+	@Test
+	public void should_show_numbers_when_countoff_given_contains7_divided_by5_not_divided_by7or3() {
+
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("275", fizzBuzz.convert(275));
+		
+	}
+	
+	@Test
+	public void should_show_fizzwhizz_when_countoff_given_contains7_divided_by3and7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("FizzWhizz", fizzBuzz.convert(147));
+		
+		
+	}
+	
+	@Test
+	public void should_show_fizz_when_countoff_given_contains7_divided_by3and5_not_divided_by7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Fizz", fizzBuzz.convert(75));
+		
+		
+	}
+	
+	@Test
+	public void should_show_Wizz_when_countoff_given_contains7_divided_by5and7_not_divided_by3() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Whizz", fizzBuzz.convert(70));
+		
+	}
+
+
+	@Test
+	public void should_show_Buzz_when_countoff_given_contains5_not_contains7_divided_by5_not_divided_by7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Buzz", fizzBuzz.convert(50));
+		
+	}
+	
+	@Test
+	public void should_show_Whizz_when_countoff_given_contains5_not_contains7_divided_by7_not_divided_by5() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Whizz", fizzBuzz.convert(56));
+		
+	}
+
+	@Test
+	public void should_show_number_when_countoff_given_contains5_not_contains7_divided_by3_not_divided_by5and7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("51", fizzBuzz.convert(51));
+		
+	}
+	
+	@Test
+	public void should_show_buzzwhizz_when_countoff_given_contains5_not_contains7_divided_by5and7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("BuzzWhizz", fizzBuzz.convert(35));
+		
+	}
+	
+	@Test
+	public void should_show_buzz_when_countoff_given_contains5_not_contains7_divided_by3and5_not_divided_by7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Buzz", fizzBuzz.convert(15));
+		
+	}
+	
+	@Test
+	public void should_show_Whizz_when_countoff_given_contains5_not_contains7_divided_by3and7_not_divided_by5() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Whizz", fizzBuzz.convert(588));
+		
+	}
+	
+	@Test
+	public void should_show_fizz_when_countoff_given_contains3_not_contains5and7() {
+		
+		 
+		FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+	    Assert.assertEquals("Fizz", fizzBuzz.convert(13));
+		
+	}
 
     @Test
-    public void fizzBuzzConvertorLeavesNormalNumbersAlone(){
+    public void should_show_number_when_countoff_given_not_contains3and5and7_cannot_divided_by3and5and7(){
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
@@ -17,7 +147,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void fizzBuzzConvertorMultiplesOfThree(){
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by3(){
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
@@ -26,7 +156,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void fizzBuzzConvertorMultiplesOfFive(){
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by5(){
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
@@ -35,7 +165,7 @@ public class FizzBuzzTest {
     }
     
     @Test
-    public void fizzBuzzConvertorMultiplesOfSevern(){
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by7(){
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
@@ -44,28 +174,31 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void multiplesOfBothThreeAndFive(){
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by3and5(){
+        
+    	FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
         Assert.assertEquals("FizzBuzz", fizzBuzz.convert(15));
     }
     
     @Test
-    public void multiplesOfBothThreeAndSeven(){
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by3and7(){
+       
+    	FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
         Assert.assertEquals("FizzWhizz", fizzBuzz.convert(21));
     }
 
     @Test
-    public void multiplesOfBothThreeAndFiveAndSeven(){
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by3and5and7(){
+        
+    	FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
         Assert.assertEquals("FizzBuzzWhizz", fizzBuzz.convert(105));
     }
 
     @Test
-    public void multiplesOfBothFiveAndSeven(){
+    public void should_show_fizz_when_countoff_given_not_contains3and5and7_can_divided_by5and7(){
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
         Assert.assertEquals("BuzzWhizz", fizzBuzz.convert(35));
